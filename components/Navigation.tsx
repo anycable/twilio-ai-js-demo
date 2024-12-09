35a1f1241ceb1f17c111f4211ebc45b3816fec94
Vladimir Dementyev <dementiev.vm@gmail.com>
2024-12-09 14:08:18 -0800

    + Github link

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { Phone, CheckSquare } from 'lucide-react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import { cn } from '@/lib/utils';
+import { Phone, CheckSquare, Github } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function Navigation() {
   const pathname = usePathname();
 
   return (
-    <nav className="flex space-x-4 mb-8">
+    <nav className="flex space-x-4 mb-8 w-full">
       <Link
         href="/"
         className={cn(
           "flex items-center space-x-2 px-4 py-2 rounded-lg transition-colors",
           pathname === "/"
             ? "bg-primary text-primary-foreground"
-            : "hover:bg-secondary"
+            : "hover:bg-secondary",
         )}
       >
         <CheckSquare className="h-4 w-4" />
@@ -28,11 +28,19 @@ export function Navigation() {
           "flex items-center space-x-2 px-4 py-2 rounded-lg transition-colors",
           pathname === "/calls"
             ? "bg-primary text-primary-foreground"
-            : "hover:bg-secondary"
+            : "hover:bg-secondary",
         )}
       >
         <Phone className="h-4 w-4" />
         <span>Call Logs</span>
+      </Link>
+      <Link
+        href="https://github.com/anycable/twilio-ai-js-demo"
+        target="_blank"
+        className="flex items-center space-x-2 px-4 py-2 rounded-lg transition-colors hover:bg-secondary !ml-auto"
+      >
+        <Github className="h-4 w-4" />
+        <span>GitHub</span>
       </Link>
     </nav>
   );
